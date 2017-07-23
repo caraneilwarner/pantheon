6133f680383af91e26135645b4e5015259bf01fb
carawarner <warnercara@gmail.com>
2017-07-23 11:07:59 -0400

    Update name

--- pantheon/scripts/name.py
+++ pantheon/scripts/name.py
@@ -1,35 +1,42 @@
+"""Scrape the webstite babynames.net to produce (name,gender,meaning) tuples.
+Write these to files for future use.
+"""
 from bs4 import BeautifulSoup
+import csv
+import json
 import requests
 
-domain = 'https://www.babynames.net'
+base_domain = 'https://www.babynames.net'
 
 def get_categories():
-    endpoint = 'https://www.babynames.net/categories'
-    links = get_pagination_links(endpoint)
+    """Scrape the homepage of babynames.net. Retrieve links to each category of
+    names. Categories are things like Arabic, Chinese, and Danish, or Aristocrat
+    and Nature, or Top 1920s and Top 1950s.
+    """
     categories = []
-
-    for link in links:
-        items = get_items(link, '.result-list')
-        for item in items:
-            try:
-                categories.append(item.contents[0].strip(' '))
-            except:
-                pass
-    return list(set(categories))
+    result = requests.get(base_domain)
+    soup = BeautifulSoup(result.text, 'lxml')
+    items = soup.select('.names-list li > a')
+    for item in items:
+        categories.append((item.contents[0].lower(), item['href']))
+    return categories
 
 
-def get_names(category):
-    endpoint = 'https://www.babynames.net/all/' + category.lower()
-    links = []
-    for i in range(22):
-        links.append(endpoint + "?page=" + str(i+1))
+def get_names_old(category):
+    """Scrape babynames.net for names in the specified category."""
+    endpoint = base_domain + [link for label,link in get_categories() if label == category.lower()][0]
+    if not endpoint: return "Invalid category"
 
-    #links = get_pagination_links(endpoint)
+    links = get_pagination_links(endpoint)
+    if len(links) == 0: links = [endpoint] # Only one page, no pagination
+
     names = []
-    
+
     for link in links:
-        items = get_items(link, '.even, .odd')
-        for item in items:
+        result = requests.get(link)
+        soup = BeautifulSoup(result.text, 'lxml')
+        items = soup.select('.even, .odd')
+        for name in items:
             try:
                 name = item.find("span", {"class":"result-name"}).contents[0]
                 gender = item.find("span", {"class":"result-gender"}).get("class")[1]
@@ -40,21 +47,74 @@ def get_names(category):
     return names
 
 
-def get_pagination_links(endpoint):
-    items = get_items(endpoint, '.pagination a')
-    links = [domain + item['href'] for item in items]
-    return links[1:] # The first is always junk
+def get_names(category):
+    """Scrape babynames.net for names in the specified category."""
+    endpoint = base_domain + [link for label,link in get_categories() if label == category.lower()][0]
+    if not endpoint: return "Invalid category"
+
+    names = scrape(endpoint)
+    return names
 
 
-def get_items(endpoint, selector):
+def scrape(endpoint):
+    names = []
     result = requests.get(endpoint)
-    soup = BeautifulSoup(result.text)
-    return soup.select(selector)
+    soup = BeautifulSoup(result.text, 'lxml')
+    items = soup.select('.even, .odd')
+    for name in items:
+        try:
+            name = item.find("span", {"class":"result-name"}).contents[0]
+            gender = item.find("span", {"class":"result-gender"}).get("class")[1]
+            desc = item.find("span", {"class":"result-desc"}).contents[0]
+            names.append((name, gender, desc))
+        except:
+            pass
+
+    # try:
+    #     next = soup.select('.pagination a')[-1]
+    #     names += scrape(next['href'])
+    # except:
+    #     pass
+
+    return names
 
 
-# Read pagination on categories to get each page
-# Go through each category page and grab title
-# Expose function for getting a list of possible categories
-# Write generic function for grabbing all names from a category
-# Go to first page of category, grab pagination, loop over pages getting name + gender
+def get_pagination_links(endpoint):
+    result = requests.get(endpoint)
+    soup = BeautifulSoup(result.text, 'lxml')
+    items = soup.select('.pagination a')[1:-1] # Skip "Previous" and "Next"
+    links = [base_domain + link['href'] for link in items]
+    return links
 
+
+def load_names(category):
+    category = category.lower()
+    if not 'json' in category:
+        # Then input is the category label
+        link = [ link for label,link in get_categories() if label == category ]
+        if not link: return "Invalid category"
+        filename = category_filename(link)
+    elif not '../../src/names' in category:
+        # Then input is the filename -- it's not relative
+        filename = '../../src/names/' + category
+    else:
+        # Then input is the correct path
+        filename = category
+
+    with open(filename, 'r') as injson:
+        names = json.load(injson)
+        return [ tuple(name.split(',')) for name in names ]
+
+
+def save_names(check=False):
+    for label,link in get_categories():
+        print("retrieving names from category: " + label)
+        names = get_names(label)
+        filename = category_filename(link)
+        with open(filename, 'w') as outjson:
+            json.dump([ ','.join(name) for name in names ], outjson)
+        if check: return # Quit early because this is a dry run
+
+
+def category_filename(link):
+    return '../src/names/%s.json' % link.rsplit('/',1)[-1]
