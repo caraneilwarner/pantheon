655fd02cf099d8f1d00471fd332379cb477b2a35
carawarner <warnercara@gmail.com>
2018-04-09 15:14:49 -0400

    Fix typo

--- pantheon/gods.py
+++ pantheon/gods.py
@@ -51,7 +51,7 @@ class God:
         Assign either XX or XY randomly with a 50/50 chance of each, unless
         <chromosomes> are passed as an argument.
         """
-        if chromosomes and chromosomes in valid_chromoomes:
+        if chromosomes and chromosomes in valid_chromosomes:
             self.chromosomes = chromosomes
         else:
             self.chromosomes = random.choice(['XX','XY'])
